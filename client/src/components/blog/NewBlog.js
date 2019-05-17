8643787d28904a01768e91190eb4f66880e006fd
tajpouria <charliswager@gmail.com>
2019-05-18 00:46:46 +0430

    can not setup useStateHook in connect hoc

--- client/src/components/blog/NewBlog.js
+++ client/src/components/blog/NewBlog.js
@@ -1,21 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 
-import Navbar from '../Navbar'
+import Navbar from '../Navbar';
+import postBlog from './postBlog'
 
-export default function() {
+export default function () {
+   const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const onTitleChange = e => {
+    e.preventDefault();
+    setTitle(e.target.value);
+  };
+
+  const onContentChange = e => {
+    e.preventDefault();
+    setContent(e.target.value);
+  };
+
+  const onFormSubmit = () => {
+    return <postBlog title={title} content={content}/>
+  };
+
   return (
     <React.Fragment>
       <Navbar path="DASHBOARD" />
-      <Form>
+      <Form onSubmit={onFormSubmit}>
         <div style={{ margin: 25 }}>
           <Form.Group controlId="exampleForm.ControlInput1">
             <Form.Label>Title</Form.Label>
-            <Form.Control type="text" placeholder="MyAwesomeBlog" />
+            <Form.Control
+              value={title}
+              onChange={onTitleChange}
+              type="text"
+              placeholder="MyAwesomeBlog"
+            />
           </Form.Group>
           <Form.Group controlId="exampleForm.ControlTextarea1">
             <Form.Label>Content</Form.Label>
-            <Form.Control as="textarea" rows="10" />
+            <Form.Control
+              value={content}
+              onChange={onContentChange}
+              as="textarea"
+              rows="10"
+            />
           </Form.Group>
           <Button variant="dark">
             <a>Done</a>
