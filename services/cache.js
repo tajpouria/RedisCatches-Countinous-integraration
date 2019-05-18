8c5bac695a47103e9d5bf5d8dce1973b7885e2a8
tajpouria <charliswager@gmail.com>
2019-05-19 03:00:30 +0430

    handle caches added

--- services/cache.js
+++ services/cache.js
@@ -1,12 +1,31 @@
+/* eslint-disable new-cap */
+/* eslint-disable func-names */
+const { createClient } = require('redis');
 const mongoose = require('mongoose');
+const util = require('util');
 
+const client = createClient('redis://127.0.0.1:6379');
+client.get = util.promisify(client.get);
 const { exec } = mongoose.Query.prototype;
 
-mongoose.Query.prototype.exec = function () {
-  const key = Object.assign({}, this.getQuery(), {
-    collection: this.mongooseCollection.name
-  });
-  console.log(key)
-
-  return exec.apply(this, arguments);
+mongoose.Query.prototype.exec = async function (...args) {
+  const key = JSON.stringify(
+    Object.assign({}, this.getQuery(), {
+      collection: this.mongooseCollection.name,
+    }),
+  );
+  const cacheValue = await client.get(key);
+  // check if have cachedValue return
+  if (cacheValue) {
+    const doc = JSON.parse(cacheValue);
+    return Array.isArray(cacheValue)
+      ? doc.map(d => new this.model(d))
+      : new this.model(doc);
+  }
+  // set data in redis
+  const result = await exec.apply(this, args);
+  client.set(key, JSON.stringify(result));
+  // if not issue query
+  await client.flushall()
+  return result;
 };
