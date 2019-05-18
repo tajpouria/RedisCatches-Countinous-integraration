fba2b3694f7d6dc3e538b53cf9153f035489596e
tajpouria <charliswager@gmail.com>
2019-05-18 19:34:48 +0430

    fix a mistake

--- note.js
+++ note.js
@@ -83,6 +83,177 @@ app.use(passport.session());
 // setup authRoutes ./routes/authRoutes.js
 
 router.get('/google', passport.authenticate('google')); // -> ./index.js require('./services/passport-config')
+
 router.get('/google/callback', (req, res) => {
   res.send(req.user); //user if accessible here
+res.redirect('/api/blogs');
 });
+
+router.get('/logout',(req, res)=>{
+  res.logOut() //remove cookies
+  res.redirect('/')
+})
+
+//##subSection One (HOC)
+
+//1. React-Router-Dom recap:
+//  >npm i react-router-dom
+
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  Link,
+  NavbarLink
+} from 'react-router-dom';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/" component={About} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+function Home() {
+  return <Link to="/about">About</Link>;
+}
+function About() {
+  return (
+    <NavbarLink to="/" className="hurry">
+      Home
+    </NavbarLink>
+  );
+}
+
+//2. React Context and Hooks basic
+
+// Create a Context ./components/MovieContext.js
+
+import React, { createContext, useState } from 'react';
+
+export const MovieContext = createContext();
+
+export const MovieProvider = props => {
+  const [movies, setMovies] = useState(['movie1', 'movie2']);
+
+  return (
+    <MovieContext.Provider value={[movies, setMovies]}>
+      {props.children}
+    </MovieContext.Provider>
+  );
+};
+
+// Wrap the Elements should access context ./App.js
+
+import React from 'react'
+
+import {MovieProvider} from './components/MovieContext'
+import MovieList from './components/MovieList'
+
+export default function(){
+  return(
+    <MovieProvider>
+      <MovieList/>
+    </MovieProvider>
+  )
+}
+
+//use values in wrapped Components ./src/components
+
+import React, {useContext} from 'react'
+
+import {MovieContext} from './movieContext'
+
+export default function(){
+  const[movies] = useContext(MovieContext)
+
+  return(
+    <div>
+    {movies.map(movie => <h1>movie</h1>)}
+    </div>
+  )
+}
+
+// useEffect
+
+import { useEffect} from 'react'
+
+//componentWillMount
+useEffect(()=>{},[])
+
+//3. HOC
+
+//hoc s will call with a composedComponent add some functionality to it and then return it ./components/requireAuth
+
+import React, {Component} from 'react';
+
+export default function(composedComponent){
+  class Authentication extends Component{
+    componentWillUpdate(nextProps){
+      const{auth:{auth}, history:{push}} = nextProps
+
+      if(!auth) push('/')
+    }
+
+    render(){
+      return <composedComponent {...this.props} />
+    }
+  }
+  return Authentication
+}
+
+//pass another component to it ./components/Resources
+
+import requireAuth from './requireAuth'
+
+const Resources =()=> <div>Access Available just for who's logged in!</div>
+
+export default requireAuth(Resources)
+
+//## SectionTwo (Data Caching with Redis)
+
+// 1.redis-cli
+// run-server >~/redis-5.0.5/src/redis-server
+// interact >~/redis-5.0.5/src/redis-cli
+
+// 2. node-redis interact 
+// >npm i redis 
+
+const redis = require('redis')
+const redisUrl = 'redis://127.0.0.1:6379'
+const client = redis.createClient(redisUrl)
+// set and get
+client.set('foo', 'bar');
+client.get('foo', console.log) //null bar
+// set and get hash
+client.hset('german','red','rot')
+client.hget('german','red', console.log) //null rot
+// set and get objects
+client.set('colors',JSON.stringify({red:'roho'}))
+client.get('colors',(err,value)=> JSON.parse(value))
+// client.flushall() 
+//expire catch
+client.set('color', 'red', 'EX', 5) //catch will expire after 5 sec.
+
+// 3. util.promisify() promisify callback
+
+const util = require('util')
+client.get = util.promisify(client.get)
+
+const value = await client.get('key')
+
+// 4. add some logic to mongoose exec
+
+const mongoose = require('mongoose')
+
+const exec = mongoose.Query.prototype.exec
+
+mongoose.Query.prototype.exec = function(){
+  console.log('IM ABOUT TO RUN QUERY')
+
+  return exec.app(this, arguments)
+}
